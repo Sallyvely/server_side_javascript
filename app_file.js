ebb472db8ecf5ea1e082096589ff8e256599e7bf
Sallyvely <tpdus4159@naver.com>
2022-05-21 13:22:48 +0900

    my web

---
+++ app_file.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+app.use(bodyParser.urlencoded({ extended: false}));
+app.locals.pretty = true;
+app.set('views','./views_file');
+app.set('view engine', 'jade');
+app.get('/topic/new',function(req,res){
+  res.render('new');
+});
+app.post('/topic',function(req,res){
+  var title = req.body.title;
+  var description = req.body.description;
+  fs.writeFile('data/'+title, description, function(err){
+    if(err){
+      res.status(500).send('Internal Sever Error');
+    }
+    res.send('Success!');
+  });
+});
+app.listen(3010, function(){
+  console.log('Connected, 3000 port!');
+});
