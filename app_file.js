aaee559e6b8ab65649e97f74270cc19b5d4d41d3
Sallyvely <tpdus4159@naver.com>
2022-05-21 18:30:41 +0900

    my web

--- app_file.js
+++ app_file.js
@@ -7,8 +7,52 @@ app.locals.pretty = true;
 app.set('views','./views_file');
 app.set('view engine', 'jade');
 app.get('/topic/new',function(req,res){
-  res.render('new');
+  fs.readdir('data',function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Sever Error');
+    }
+  res.render('new',{topics:files});
+})
+})
+;
+app.get(['/topic','/topic/:id'],function(req,res){
+  fs.readdir('data',function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Sever Error');
+    }
+    var id = req.params.id;
+    if(id){
+      fs.readFile('data/'+id,'utf8',function(err,data){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Sever Error');
+        }
+        res.render('view',{topics:files, title: id, description:data});
+      })
+    }
+    else{
+      res.render('view',{topics:files,title: 'Welcome', description:'Hello, Javascript for server.'});
+    }
+  })
 });
+// app.get('/topic/:id',function(req,res){
+//   var id = req.params.id;
+//   fs.readdir('data',function(err, files){
+//     if(err){
+//       console.log(err);
+//       res.status(500).send('Internal Sever Error');
+//     }
+//   fs.readFile('data/'+id,'utf8',function(err,data){
+//     if(err){
+//       console.log(err);
+//       res.status(500).send('Internal Sever Error');
+//     }
+//     res.render('view',{topics:files,title: id, description:data});
+//   })
+// })
+// })
 app.post('/topic',function(req,res){
   var title = req.body.title;
   var description = req.body.description;
@@ -16,7 +60,7 @@ app.post('/topic',function(req,res){
     if(err){
       res.status(500).send('Internal Sever Error');
     }
-    res.send('Success!');
+    res.redirect('/topic/'+title);
   });
 });
 app.listen(3010, function(){
